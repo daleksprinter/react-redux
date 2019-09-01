27fdcf452aace51ed076f392aaaa7d992823f09d
daleksprinter <b1015120@fun.ac.jp>
2019-09-01 14:04:31 +0900

    update todo component and list todos

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,17 +5,35 @@ class Todo extends React.Component {
     constructor(){
         super();
         this.state = {
-            todo : []
+            todo_txt : ""
         }
     }
 
+    handleChange = (e) => {
+        this.setState({
+            todo_txt : e.target.value
+        })
+    }
 
+    handleClick = () => {
+        this.props.addTodo(this.state.todo_txt);
+        this.setState({
+            todo_txt : ""
+        })
+    }
 
     render(state, props) {
-        console.log(this.props);
         return (
         <div>
-            hoge
+            <textarea onChange = {this.handleChange} value = {this.state.todo_txt}></textarea>
+            <button onClick = {this.handleClick}>ADD</button>
+            <ul>
+                {this.props.todo.todoList.map((todo, index) => {
+                    return (
+                        <li key = {index}>{todo}</li>
+                    )
+                })}
+            </ul>
         </div>
         );
     }
