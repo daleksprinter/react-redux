0286d9802154e53fbcf7002e0a94837e1609107a
daleksprinter <b1015120@fun.ac.jp>
2019-09-01 14:18:03 +0900

    add subtodo(add todo to same store)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Todo from './containers/Todo'
+import Todo from './containers/Todo';
+import SubTodo from './containers/SubTodo';
 
 function App() {
   return (
     <div>
       <Todo />
+      <SubTodo />
     </div>
   );
 }
